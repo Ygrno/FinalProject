1731744b3681e3a1253686c58db33cedbfc50818
Ygrno <yakirgre@post.bgu.ac.il>
2021-04-25 14:33:45 +0300

    Organize files + axios search & update

--- front_hila/src/user.mock.js
+++ front_hila/src/user.mock.js
@@ -1,14 +1,14 @@
 import { UserType } from "./constants";
 
-/*
+
 const user = null;
 export default user;
-*/
 
-export default {
-    name: 'הילה',
-    userType: UserType.Soldier
-}
+
+// export default {
+//     name: 'הילה',
+//     userType: UserType.Soldier
+// }
 
 
 
