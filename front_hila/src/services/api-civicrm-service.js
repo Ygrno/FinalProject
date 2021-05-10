505c48eb4bce3ae208d0ecc917001ceac9e040d0
Ygrno <yakirgre@post.bgu.ac.il>
2021-05-10 17:35:11 +0300

    added search function to api-civicrm-service

--- front_hila/src/services/api-civicrm-service.js
+++ front_hila/src/services/api-civicrm-service.js
@@ -13,3 +13,48 @@ export const updateContact = (email, api_key) => {
     return client.get(`${updateUrl}${urlParams}`)
 };
 
+/*
+Input: User's API-Key and Search Data
+Search Data should look like this:
+
+let search_data = {
+    contact_id: '',
+    email: '',
+    contact_type: '',
+    first_name: '',
+    last_name: '',
+}
+
+For a full search of all contacts the user can see - 
+leave all the fields blank (empty string, like in the example above)
+Filling the Contact ID / Email fields should return only 1 record from the DB.
+
+Using an API-Key of a user that isn't administrator (like Soldiers, volunteers)
+should return only 1 Record at most (which is the user's own data - usful for profile)
+
+--------------------------------------------------------------------------------------------
+
+Output: JSON:
+{
+    is_error:,
+    version:,
+    count:,
+    //OPTIONAL (ID, CONTACT_TYPE .... (the input fields themselves))
+    values: [{},{},{}] //The Actual results of the people that were found
+}
+
+Wrong or Problomatic input will return the civicrm error message explaining what was wrong.
+*/
+export const serachContacts = (search_data, api_key) => {
+    var urlParams = new URLSearchParams({
+        'entity': 'Contact',
+        'action': 'get',
+        'json': JSON.stringify({"sequential": 1,"id": search_data.contact_id,
+                            "email": search_data.email, "contact_type": search_data.contact_type, 
+                            "first_name": search_data.first_name, "last_name": search_data.last_name}),
+        'api_key': api_key,
+        'key': globals.SITE_KEY
+    })
+    return client.get(`${updateUrl}${urlParams}`)
+}
+
