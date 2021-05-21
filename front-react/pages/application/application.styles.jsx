ca45d381415d81370b2b764d25b5442f905bbe8c
jeniaka <kevgeny@post.bgu.ac.il>
2021-05-21 14:42:21 +0300

    profile page fixes

--- front-react/pages/application/application.styles.jsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "antd";
-import styled from "styled-components";
-
-export const ApplicationPageContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    color: red;
-
-`
